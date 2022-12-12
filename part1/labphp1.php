007f7aa5c8655789155cd1e3450ba0418e46cae4
Aya Eid ♥ <ayaeid783@yahoo.com>
2022-12-12 19:30:06 +0200

    newedit

--- part1/labphp1.php
+++ part1/labphp1.php
@@ -1,87 +1,105 @@
 <?php
+$nameErr = $EmailErr = $genderErr  = $agreeErr = "";
+$name = $email = $group = $gender = $courses = $agree = $details="";
 
-$nameErr = $EmailErr = $genderErr  = $agreeErr = "";   
-
-if ($_SERVER["REQUEST_METHOD"] == "GET") {  
-    
-    if (empty($_GET["name"])) {  
-         $nameErr = "Name is required";  
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+  if (empty($_GET["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = $_GET["name"];
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
     }
-    if (empty($_GET["mail"])) {  
-      $EmailErr = "Email is required";  
-   }
-   if (empty($_GET["gender"])) {  
-      $genderErr = "Gender is required";  
-   }
-   if (empty($_GET["agree"])) {  
-      $agreeErr = "You must agree to terms";  
-   }
+  }
+  
+  if (empty($_GET["email"])) {
+    $EmailErr = "Email is required";
+  } else {
+    $email = $_GET["email"];
+  }
+  if(!empty($_GET["group"])) {
+    $group = $_GET["group"];
+ }
+    
+ if(!empty($_GET["details"])) {
+    $details = $_GET["details"];
+ }
 
-   }
+  if (empty($_GET["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = $_GET["gender"];
+  }
+  if (empty($_GET["agree"])) {  
+    $agreeErr = "You must agree to terms";  
+ }else {
+    $agree =  $_GET["agree"];
+  }
+}
+
 ?>
 
-
+<!DOCTYPE HTML>  
 <html>
-   <head>
-      <style>
-         .error {color: #FF0000;}
-         input{margin-left:60px;}
-         textarea{margin-left:20px;}
-      </style>
-   </head>
+<head>
+<style>
+.error {color: #FF0000;}
+input{margin-left:60px;}
+textarea{margin-left:20px;}
+</style>
+</head>
+<body> 
 
+<h2>Application name : AAST_BIS class registration</h2>
 
-   <body>
-      <h2>Application name : AAST_BIS class registration</h2>
-      <p><span class="error">* Required field.</span></p>
-   
-      <form action = "<?php $_PHP_SELF ?>" method = "GET">
-         Name: <input type = "text" name = "name" /><span class="error">* <?php echo $nameErr;?> </span> <br>
-         E-mail: <input type = "email" name = "mail" /> <span class="error">* <?php echo $EmailErr;?> </span> <br>
-         Group#  <input type = "text" name = "group" /> <br>
-         Class details:  <textarea name="details" cols="50" rows="5"></textarea> <br>
-         Gender: <input type="radio" name="gender" value="Female" />Female
-         <input type="radio" name="gender" value="Male" />Male <span class="error">* <?php echo $genderErr;?> </span> <br>
-         <label for="courses">Select courses:</label>
-         <select name='courses[]' multiple>
-            <option>  PHP</option>
+<p><span class="error">* Required field.</span></p>
+
+<form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $EmailErr;?></span>
+  <br><br>
+  Group# : <input type = "text" name = "group" /> 
+  <br><br>
+  Class details: <textarea name="details" cols="40" rows="5"></textarea> 
+  <br><br>
+  Gender: <input type="radio" name="gender" <?php if (isset($gender) && $gender=="Female") echo "checked";?> value="Female" />Female
+   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="Male" />Male 
+   <span class="error">* <?php echo $genderErr;?> </span> 
+   <br><br>
+   <label for="courses">Select courses:</label>
+   <select name='courses[]' multiple>
+            <option> PHP</option>
             <option>Javascript</option> 
             <option>HTML</option>
             <option>MYSQL</option>
-         </select> <br>
-         <label for="agree">Agree</label>  
-         <input type="checkbox" name="agree" > <span class="error">* <?php echo $agreeErr;?> </span> <br>
-         
-         <input type = "submit" name="submit" />
-      </form>
-         
-   </body>
-</html>
+    </select> 
+    <br><br>
+    <label for="agree">Agree</label>  
+    <input type="checkbox" name="agree" <?php if (!empty($agree)) echo "checked";?>> 
+    <span class="error">* <?php echo $agreeErr;?> </span> 
+    <br><br>
+    <input type = "submit" name="submit" value="Submit"/>
+ 
+</form>
+
 <?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $group;
+echo "<br>";
+echo $details;
+echo "<br>";
+echo $gender;
+echo "<br>";
+echo $courses;
 
-if(!empty($_GET["name"])) {
-   echo "<h2>Your given values are as:</h2>";
-    echo "Name: ". $_GET['name']. "<br />";
- }
- if(!empty($_GET["mail"])) {
-   echo "E-mail: ". $_GET['mail']. "<br />";
-}
-
- if(!empty($_GET["group"])) {
-   echo "Group#: ". $_GET['group']. "<br />";
-}
-if(!empty($_GET["details"])) {
-   echo "Class details: ". $_GET['details']. "<br />";
-}
-
-if(!empty($_GET["gender"])) {
-   echo "Gender: ". $_GET['gender']. "<br />";
-}
-if(!empty($_GET["courses"]))
-        {
-echo "Your courses are :" ;         
-foreach ($_GET['courses'] as $courses)
-echo " $courses";  
-        }
-            
 ?>
+
+</body>
+</html>
